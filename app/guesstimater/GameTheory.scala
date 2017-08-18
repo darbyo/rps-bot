45bf203098f7b8fe025524344188d7d2331b9ec6
Kurt Logan <kurt.logan@digital.hmrc.gov.uk>
2017-08-18 14:01:50 +0100

    Finished Game theory

--- app/guesstimater/GameTheory.scala
+++ app/guesstimater/GameTheory.scala
@@ -3,6 +3,8 @@ package guesstimater
 import models.{Move, Play, Result}
 
 trait GameTheory {
+  val randomPick: Random
+
   def predict(play: Play) = {
     if(play.result == Some(Result.LOSE)) {
       if(play.opponentMove == Some(Move.PAPER)){
@@ -12,12 +14,17 @@ trait GameTheory {
       } else if (play.opponentMove == Some(Move.DYNAMITE)) {
         Move.WATERBOMB
       } else Move.ROCK
-  } else {
+  } else if(play.result == Some(Result.WIN)) {
       if(play.ourMove == (Move.PAPER)) {
         Move.ROCK
+      }else if(play.ourMove == Move.SCISSORS) {Move.PAPER}
+      else if (play.ourMove == Move.WATERBOMB){
+        Move.DYNAMITE
       }
-      else
+      else {
       Move.SCISSORS
+      }
    }
+    else randomPick.predict
   }
 }
