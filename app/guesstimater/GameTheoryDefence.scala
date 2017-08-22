eab851bfccb78012fe103f5d4365a8c90e955f43
Rob Darby <rob.darby@digital.hmrc.gov.uk>
2017-08-22 14:20:24 +0100

    Draw random if draw in game theory defence

--- app/guesstimater/GameTheoryDefence.scala
+++ app/guesstimater/GameTheoryDefence.scala
@@ -1,9 +1,12 @@
 package guesstimater
 
-import models.Play
+import models.{Play, Result}
 
 trait GameTheoryDefence {
+
+  val playRandom: Random
+
   def predict(play: Play) = {
-    play.opponentMove.get
+    if(play.result.contains(Result.DRAW)) playRandom.predict else play.opponentMove.get
   }
 }
